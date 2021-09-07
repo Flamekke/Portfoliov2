21bf4d906a4bf69d2aaea3733c8a01099a688e6f
Valounte <valentin.lyon@epitech.eu>
2021-09-07 19:58:57 +0200

    home page

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,15 @@
+import './Home.css'
+import React, {useEffect, useState, useRef} from "react";
+
+
+
+const Home = (props) => {
+    return(
+        <div>
+            <h1>Hey, I'm <span>Valentin</span></h1>
+            <h2>A <span>student</span> in part-time internship <br/>and a <span>Back-End developer</span>.</h2>
+        </div>
+    );
+}
+
+export default Home;
