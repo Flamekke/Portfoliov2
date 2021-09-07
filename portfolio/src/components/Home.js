aea249a0f14c69dfaafc773482274f0e22d8ec7d
Valounte <valentin.lyon@epitech.eu>
2021-09-07 21:28:14 +0200

    anim

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,16 +1,34 @@
 import './Home.css'
 import React, {useEffect, useState, useRef} from "react";
+import Lottie from 'react-lottie';
+import HomeAnimation from '../asset/18123-developer.json';
 
 
 
 const Home = (props) => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: HomeAnimation,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+
+
     return(
         <div>
             <h1>Hey, I'm <span>Valentin</span></h1>
-            <h2>A <span>student</span> in part-time internship <br/>and a <span>Back-End developer</span>.</h2>
-            <button class="button">Click Me</button>
-
-        </div>
+            <h2>A <span>student</span> in part-time internship <br />and a <span>Back-End developer</span>.</h2>
+            <div><button className="button">See more</button></div>
+            <div className="anim">
+            <Lottie 
+                options={defaultOptions}
+                // height={450}
+                // width={450}
+            /></div>
+    </div>
     );
 }
 
