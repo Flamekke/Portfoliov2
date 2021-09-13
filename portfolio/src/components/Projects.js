6edd91e7fa44bc4a16e65372ea3b053678e48537
Valounte <valentin.lyon@epitech.eu>
2021-09-13 21:00:31 +0200

    d

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,20 +1,56 @@
 import './Projects.css'
 import React from "react";
-import {Carousel} from '3d-react-carousal';
+import appitech from '../asset/Appitech.jpg';
 
 
 class Projects extends React.Component {
     
     render() {
-      let slides = [
-        <div> <img  src="https://picsum.photos/800/300/?random" alt="1" />tessssst </div>,
-      <img  src="https://picsum.photos/800/301/?random" alt="2" />  ,
-      <img  src="https://picsum.photos/800/302/?random" alt="3" />  , 
-      <img  src="https://picsum.photos/800/303/?random" alt="4" />  ,
-      <img src="https://picsum.photos/800/304/?random" alt="5" />  ];
+ 
       return (
         <div>
-          <Carousel slides={slides}/>
+
+<div class="container10" style={{marginTop:70}}>
+
+<div class="card10">
+		<figure class="card__thumb">
+			<img src={appitech} style={{height:600}} class="card__image"/>
+			<figcaption class="card__caption">
+				<div class="card__title">Appitech</div>
+				<div class="card__snippet">Intranet of my school, mobile application version, view your schedule, profile, projects, modules, notifications...<br/>React Native / PHP Laravel</div>
+				{/* <a href="" class="card__button">Read more</a> */}
+			</figcaption>
+		</figure>
+	</div>
+
+
+
+  <div class="card10">
+		<figure class="card__thumb">
+			<img src="https://source.unsplash.com/qXMpNtNp1uE/300x510" alt="Picture by Daniel Lincoln" class="card__image"/>
+			<figcaption class="card__caption">
+				<div class="card__title">Why You Should Bring Your Dog To Work</div>
+				<div class="card__snippet">On Friday, offices around the country celebrated the 15th annual Take Your Dog to Work Day. Though the event's primary goal is to raise awareness for pet adoption, the unanticipated impact may be a slightly more relaxing work environment for any office choosing to participate.</div>
+				{/* <a href="" class="card__button">Read more</a> */}
+			</figcaption>
+		</figure>
+	</div>
+
+
+	<div class="card10">
+		<figure class="card__thumb">
+			<img src="https://source.unsplash.com/qXMpNtNp1uE/300x510" alt="Picture by Daniel Lincoln" class="card__image"/>
+			<figcaption class="card__caption">
+				<div class="card__title">Why You Should Bring Your Dog To Work</div>
+				<div class="card__snippet">On Friday, offices around the country celebrated the 15th annual Take Your Dog to Work Day. Though the event's primary goal is to raise awareness for pet adoption, the unanticipated impact may be a slightly more relaxing work environment for any office choosing to participate.</div>
+				{/* <a href="" class="card__button">Read more</a> */}
+			</figcaption>
+		</figure>
+	</div>
+</div>
+
+
+
         </div>
       );
     }
